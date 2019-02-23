1872bc609383e10f3af03bf279ed02616a44b367
Vaibhav-Leo <vaibhav.shringarpure@jellyfish.co.uk>
2019-02-23 17:43:28 +0000

    test

---
+++ app/components/Cards/Cards.jsx
@@ -0,0 +1,37 @@
+/* Component  : Cards is a Functional Component
+ * Created on : 23/2/2019 17:42:47
+ * Created by : Vaibhav Shringarpure
+ */
+
+/* Import Statement Below */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/* Type Checking for PropTypes */
+const propTypes = {
+  // id: Proptypes.number.isRequired,
+  // name: Proptypes.string,
+}
+
+/* Declare default Props value for all non-required props */
+const defaultProps = {
+  // name: 'Vaibhav Shringarpure',
+}
+
+
+/* Functional Component Function below */
+const Cards = (props) => {
+
+	//Define your Methods Here
+
+  
+  //Returns JSX below
+  return (
+		<div>Hello</div>
+	)
+}
+
+Cards.propTypes = propTypes; // from Line 11
+Cards.defaultProps = defaultProps; // from Line 17
+
+export default Cards;
